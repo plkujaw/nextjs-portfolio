45da64536c27b71bc97a1f78588c9cfe30cca9c4
plkujaw <kuba.jawor@gmail.com>
2023-12-21 17:41:01 +0100

    clean

--- out/_next/static/h-VgzPsEv7-47oqg8J1Vz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/771-ad7b6b534bfaf6c2.js","static/chunks/pages/index-d8759b622b24a986.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
