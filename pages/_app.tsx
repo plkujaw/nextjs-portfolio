dddf8c71d9f19d3c341660880409a788c408b775
plkujaw <kuba.jawor@gmail.com>
2023-12-28 11:56:00 +0000

    favicon update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -81,10 +81,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link
+        {/* <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥷🏼</text></svg>"
-        />
+        /> */}
         <title>Home | kubajawor.dev</title>
         <meta
           name="description"
