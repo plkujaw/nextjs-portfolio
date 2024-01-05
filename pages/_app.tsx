fba4cac0d8f2d345baf2a26492c1ef8939d2365c
plkujaw <kuba.jawor@gmail.com>
2024-01-05 11:06:33 +0000

    metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
