4592e75567124a95b92e4b8bb7e4771d89bfd4e6
plkujaw <kuba.jawor@gmail.com>
2024-01-05 09:38:28 +0000

    metadata test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,10 @@ import Header from '@/components/Header';
 import Intro from '@/components/Intro';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
-import { Metadata } from 'next';
 import projects from '@/data/projects.js';
 import otherWorks from '@/data/otherWorks.js';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
-export const metadata: Metadata = {
-  title: '...',
-  description: '...',
-};
 
 export default function Home() {
   return (
